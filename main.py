2aa288579f51f72dd0795e84d632c3af5ac196f8
Rosalbino Bisignano <ros.bisignano@gmail.com>
2026-03-21 00:01:58 +0100

    Add project structure, ingestion, state, chains, nodes, graph

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+from graph.graph import app
 
 def main():
-    print("Hello from langchain-course!")
+    print("Hello advanced RAG!")
+    print(app.invoke(input={"question": "what is agent memory?"}))
 
 
 if __name__ == "__main__":
